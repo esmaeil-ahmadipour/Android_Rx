98bdce2817b4e4942bb00d57d0580c837d13cc13
Esmaeil Ahmadipour <software8686@gmail.com>
2020-03-25 21:17:36 +0430

    `commit> Samples> 09> RxAndroid And Observable.timer()

--- app/src/main/java/ir/ea2/android_rx/MainActivity.java
+++ app/src/main/java/ir/ea2/android_rx/MainActivity.java
@@ -2,56 +2,47 @@ package ir.ea2.android_rx;
 
 import android.os.Bundle;
 import android.util.Log;
-
 import java.util.concurrent.TimeUnit;
-
 import androidx.appcompat.app.AppCompatActivity;
 import io.reactivex.rxjava3.android.schedulers.AndroidSchedulers;
 import io.reactivex.rxjava3.annotations.NonNull;
 import io.reactivex.rxjava3.core.Observable;
 import io.reactivex.rxjava3.core.Observer;
 import io.reactivex.rxjava3.disposables.Disposable;
-import io.reactivex.rxjava3.functions.Predicate;
 import io.reactivex.rxjava3.schedulers.Schedulers;
 
 public class MainActivity extends AppCompatActivity {
-    public static final String TAG = "TAG_LOG2";
-
+    public static final String TAG = "TAG_LOG";
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        Observable<Long> longObservable = Observable.interval(1, TimeUnit.SECONDS)
+
+        Observable<Long> observable = Observable.timer(10,TimeUnit.SECONDS)
                 .subscribeOn(Schedulers.io())
-                .takeWhile(new Predicate<Long>() {
-                    @Override
-                    public boolean test(Long aLong) throws Throwable {
-
-                        return aLong <= 5;
-                    }
-                })
                 .observeOn(AndroidSchedulers.mainThread());
-        longObservable.subscribe(new Observer<Long>() {
-            @Override
-            public void onSubscribe(@NonNull Disposable d) {
+        Log.e(TAG, "Start Now .." );
 
-            }
+     observable.subscribe(new Observer<Long>() {
+         @Override
+         public void onSubscribe(@NonNull Disposable d) {
 
-            @Override
-            public void onNext(@NonNull Long aLong) {
-                Log.e(TAG, aLong.toString());
-            }
+         }
 
-            @Override
-            public void onError(@NonNull Throwable e) {
+         @Override
+         public void onNext(@NonNull Long aLong) {
+            Log.e(TAG, "Finish After 10 Seconds .." );
+         }
 
-            }
+         @Override
+         public void onError(@NonNull Throwable e) {
 
-            @Override
-            public void onComplete() {
+         }
 
-            }
-        });
+         @Override
+         public void onComplete() {
 
+         }
+     });
     }
 }
