3129e4ac433d94baf7db7d893a2dc78f809cede7
Esmaeil Ahmadipour <software8686@gmail.com>
2020-03-25 20:42:13 +0430

    `commit> Samples> 08> RxAndroid And Observable.interval() And takeWhile()

--- app/src/main/java/ir/ea2/android_rx/MainActivity.java
+++ app/src/main/java/ir/ea2/android_rx/MainActivity.java
@@ -3,53 +3,55 @@ package ir.ea2.android_rx;
 import android.os.Bundle;
 import android.util.Log;
 
+import java.util.concurrent.TimeUnit;
+
 import androidx.appcompat.app.AppCompatActivity;
 import io.reactivex.rxjava3.android.schedulers.AndroidSchedulers;
 import io.reactivex.rxjava3.annotations.NonNull;
-import io.reactivex.rxjava3.core.Flowable;
 import io.reactivex.rxjava3.core.Observable;
 import io.reactivex.rxjava3.core.Observer;
 import io.reactivex.rxjava3.disposables.Disposable;
+import io.reactivex.rxjava3.functions.Predicate;
 import io.reactivex.rxjava3.schedulers.Schedulers;
 
 public class MainActivity extends AppCompatActivity {
-    public static final String TAG = "TAG_LOG";
+    public static final String TAG = "TAG_LOG2";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        // * We Get Error because 1000000 is out of range For Integer-Value's .
-        // * Using Flowable For Cover The (Out Of Memory Exception) .
-        Flowable<Integer> integerFlowable = Flowable.range(1,1000000)
-                .observeOn(Schedulers.computation())
-                .onBackpressureBuffer();
-
-
-
-        Observable<Integer> integerObservable = integerFlowable.toObservable();
-        integerObservable.subscribeOn(Schedulers.io())
-                .observeOn(AndroidSchedulers.mainThread())
-                .subscribe(new Observer<Integer>() {
+        Observable<Long> longObservable = Observable.interval(1, TimeUnit.SECONDS)
+                .subscribeOn(Schedulers.io())
+                .takeWhile(new Predicate<Long>() {
                     @Override
-                    public void onSubscribe(@NonNull Disposable d) {
+                    public boolean test(Long aLong) throws Throwable {
 
+                        return aLong <= 5;
                     }
+                })
+                .observeOn(AndroidSchedulers.mainThread());
+        longObservable.subscribe(new Observer<Long>() {
+            @Override
+            public void onSubscribe(@NonNull Disposable d) {
 
-                    @Override
-                    public void onNext(@NonNull Integer integer) {
-                        Log.e(TAG , integer.toString());
-                    }
+            }
 
-                    @Override
-                    public void onError(@NonNull Throwable e) {
+            @Override
+            public void onNext(@NonNull Long aLong) {
+                Log.e(TAG, aLong.toString());
+            }
 
-                    }
+            @Override
+            public void onError(@NonNull Throwable e) {
 
-                    @Override
-                    public void onComplete() {
+            }
 
-                    }
-                });
+            @Override
+            public void onComplete() {
+
+            }
+        });
+
     }
 }
